885e1fff42f9816cb81784f06795c4d39976ba30
donvine mugendi <donvinemugendi@gmail.com>
2023-08-17 21:07:10 +0300

    completed lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,53 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by_id(params[:user_id])
+      if user 
+        items = user.items
+        render json: items
+      else
+        render_not_found_response
+      end
+      
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
 
+  def show
+    item = Item.find_by_id(params[:id])
+    if item
+      render json: item, status: :ok
+    else
+      render_not_found_response
+    end
+  end
+  
+
+  def create
+    if params[:user_id]
+      user = User.find_by(params[:user_id])
+      if user
+        user.items.create(params_hash)
+        render json: user.items.last, status: :created
+      else
+        render_not_found_response
+      end
+
+    else
+      item = Item.create(params_hash)
+      render json: item, status: :created
+    end
+  end
+
+  private
+
+  def params_hash
+    params.permit(:user_id, :description, :name, :price)
+  end
+  def render_not_found_response
+    render json: { error: 'Not Found' }, status: :not_found
+  end
 end
